6ab11c9d543ed377c9578a25e716199cd9592f7f
eduardo704 <eduardo704@gmail.com>
2017-01-25 09:19:19 -0200

    change route name to slides

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,4 @@
-const noteRoutes = require('./note_routes');
+const noteRoutes = require('./slide_routes');
 module.exports = function(app, db) {
   noteRoutes(app, db);
   // Other route groups could go here, in the future
